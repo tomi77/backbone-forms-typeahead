dbc8beaa339972501171d82582176c44fde70fe6
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-07-14 08:25:48 +0200

    new build system

--- dist/bbf-typeahead.amd.js
+++ dist/bbf-typeahead.amd.js
@@ -1,14 +1,12 @@
-
-(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(["underscore","backbone","backbone-forms","bootstrap-typeahead"], factory);
-  } else if (typeof exports === 'object') {
-    module.exports = factory(require('underscore'), require('backbone'), require('backbone-forms'), require('bootstrap-typeahead'));
-  } else {
-    root.bbf_typeahead = factory(root._, root.Backbone, root.Backbone.Forms, root.ta);
-  }
-}(this, function(_, Backbone, _Form, _ta) {
-
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(["underscore", "backbone", "backbone-forms", "bootstrap-typeahead"], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('underscore'), require('backbone'), require('backbone-forms'), require('bootstrap-typeahead'));
+    } else {
+        root.bbf_typeahead = factory(root._, root.Backbone);
+    }
+}(this, function (_, Backbone) {
 var Base, Form, Text;
 
 Form = Backbone.Form;
@@ -95,7 +93,4 @@ Form.editors['bootstrap.typeahead'] = Text.extend({
     this.setValue(this.value);
   }
 });
-
-return null;
-
 }));
